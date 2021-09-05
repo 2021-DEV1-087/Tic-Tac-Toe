52a1c041bc4cc2d30111be54dff80d83c91bb11e
db2admin <zaineb.dimassi@esprit.tn>
2021-09-05 20:17:09 +0200

    unit test for 2 alternatives players , unit test for general condition of the board (9 squares, 3 rows and 3 columns), ajust the method in the test of PlayerX is first

--- src/test/java/com/game/TicTacToe/TicTacToeApplicationTests.java
+++ src/test/java/com/game/TicTacToe/TicTacToeApplicationTests.java
@@ -30,7 +30,29 @@ class TicTacToeApplicationTests {
 	@Test
 	void whenStartGameThePlayerXShouldStartFirstTest() {
 		// game is empty
-		assertThrows(FirstPlayerException.class, () -> game.play(playerO));
+		assertThrows(FirstPlayerException.class, () -> game.play(0,2, playerO));
+	}
+
+	//test Players alternate placing X’s and O’s on the board
+	@Test
+	void whenNextPlayerIsTheSamethanThePreviousOneThenThrowsNextPlayerIsTheSameOneException() {
+		game.play(1, 1, playerX); // current Player now is playerX
+		assertThrows(NextPlayerIsTheSameOneException.class, () -> game.play(2, 2, playerX));
+	}
+
+	//Players cannot play on a played position.
+	@Test
+	void whenMarkerBoundWasOcuppiedThenThrowsPositionAlreadyOccupiedException() {
+		game.play(3, 3, playerX);
+		assertThrows(PositionAlreadyOccupied.class, () -> game.play(3, 3, playerO));
+	}
+
+	/* Players alternate placing X’s and O’s on the board until either:
+      One player has three in a row, All nine squares are filled and.
+      This test with 5 rows and columns and 25 squares*/
+	@Test
+	void whenMarkerBoundWasOutsideOfBoundThenThrowsMarkerOutsideBoardException() {
+		assertThrows(PositionOutsideBoardException.class, () -> game.play(5, 5, playerX));
 	}
 }
 
