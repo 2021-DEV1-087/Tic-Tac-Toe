c748335f32b44b084a86ad1dfebb6bc92c720a30
db2admin <zaineb.dimassi@esprit.tn>
2021-09-05 21:24:06 +0200

    unit test for the gaming status: inProgress, Over with and without winner, Draw

--- src/test/java/com/game/TicTacToe/TicTacToeApplicationTests.java
+++ src/test/java/com/game/TicTacToe/TicTacToeApplicationTests.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
@@ -56,6 +57,106 @@ class TicTacToeApplicationTests {
 	@Test
 	void whenMarkerBoundWasOutsideOfBoundThenThrowsMarkerOutsideBoardException() {
 		assertThrows(PositionOutsideBoardException.class, () -> game.play(5, 5, playerX));
+	}
+
+	/* scenario :Board is full ,game over with Winner, diagonal from right line
+       0 O X
+	   O X X
+	   X O X
+    */
+	@Test
+	void whenBoardIsFullWithWinnerThenGameIsOver() {
+		game.play(1, 3, playerX); // the Player X is always the First
+		game.play(1, 1, playerO); // alternative player
+		game.play(2, 3, playerX);
+		game.play(1, 2, playerO);
+		game.play(1, 3, playerX);
+		game.play(2, 2, playerX);
+		game.play(2, 1, playerO);
+		game.play(3, 1, playerX);
+		game.play(3, 2, playerO);
+		game.play(3, 3, playerX);
+		assertEquals(game.getGameStatus(), GameStatus.OVER);
+
+	}
+	/* scenario :board not completely full , game over with winner, first vertical line
+       X O O
+       X . .
+       X . .
+    */
+	@Test
+	void whenOneOfPlayerFillUpThreeVerticalSuccessiveMarkersThenGameIsOver() {
+		game.play(1, 1, playerX); // the Player X is always the First
+		game.play(1, 2, playerO);// alternative player
+		game.play(2, 1, playerX);
+		game.play(1, 3, playerO);
+		game.play(3, 1, playerX);
+		assertEquals(game.getGameStatus(), GameStatus.OVER);
+	}
+	/* scenario: board not completely full , game over with winner, first horizontal line
+       X X X
+	   . O .
+	   . O .
+    */
+	@Test
+	void whenOneOfPlayerFillUpThreeHorizontalSuccessiveMarkersThenGameIsOver() {
+		game.play(1, 1, playerX); // the Player X is always the First
+		game.play(2, 2, playerO); // alternative player
+		game.play(1, 2, playerX);
+		game.play(3, 2, playerO);
+		game.play(1, 3, playerX);
+		assertEquals(game.getGameStatus(), GameStatus.OVER);
+	}
+	/* scenario :board is full and game over without winner => Draw
+       O X X
+	   X O O
+	   X O X
+    */
+	@Test
+	void whenBoardIsFullWithoutWinnerThenGameIsDraw() {
+		game.play(1, 2, playerX); // the Player X is always the First
+		game.play(1, 1, playerO); // alternative player
+		game.play(1, 3, playerX);
+		game.play(2, 2, playerO);
+		game.play(2, 1, playerX);
+		game.play(2, 3, playerO);
+		game.play(3, 1, playerX);
+		game.play(3, 2, playerO);
+		game.play(3, 3, playerX);
+		assertEquals(game.getGameStatus(), GameStatus.DRAW);
+
+	}
+	/* scenario :board not completely full, game over with winner, diagonal from left line
+       X . .
+	   O X .
+	   . O X
+    */
+
+	@Test
+	void whenOneOfPlayerFillUpThreeDiagonalFromLeftSuccessiveMarkersThenGameIsOver() {
+		game.play(1, 1, playerX); // the Player X is always the First
+		game.play(2, 1, playerO);// alternative player
+		game.play(2, 2, playerX);
+		game.play(3, 2, playerO);
+		game.play(3, 3, playerX);
+		assertEquals(game.getGameStatus(), GameStatus.OVER);
+	}
+
+
+	/* scenario :board not completely full , game over with Winner, diagonal from right line
+       . . X
+       O X .
+       X O .
+    */
+	@Test
+	void whenOneOfPlayerFillUpThreeDiagonalRightSuccessiveMarkersThenGameisOver() {
+		game.play(1, 3, playerX); // First Pleyer now is playerX
+		game.play(2, 1, playerO);
+		game.play(2, 2, playerX);
+		game.play(3, 2, playerO);
+		game.play(3, 1, playerX);
+		assertEquals(game.getGameStatus(), GameStatus.OVER);
+
 	}
 }
 
